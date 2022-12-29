dd1814151368cf1ff9ab6726c2b80f94445ec910
Alice Liu <amliu@umich.edu>
2022-12-28 22:13:28 -0600

    update redirect url

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@
 // https://developer.spotify.com/dashboard/applications
 
 export const CLIENTID = '5230258282ca42bfb5ab68558b0a4f27';
-export const REDIRECTURL = 'https://moodymusicapp.herokuapp.com/';
+export const REDIRECTURL = 'https://moodymusic.onrender.com/';
